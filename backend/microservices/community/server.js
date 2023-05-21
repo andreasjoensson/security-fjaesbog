87757d8563e4610a2143bf65101a2ab8e2fb2baa
Andreas Moreno Jønsson <andreas.ecuador@live.dk>
2023-05-21 14:02:38 +0200

    Ny port til community

--- backend/microservices/community/server.js
+++ backend/microservices/community/server.js
@@ -23,7 +23,7 @@ const {
   const app = express();
   server.applyMiddleware({ app });
 
-  const PORT = process.env.PORT || 8000;
+  const PORT = process.env.PORT || 3000;
 
   app.listen({ port: PORT }, () => rabbitMqListeners());
 })();
