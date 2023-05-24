2a52f087cf04154d7075f48592e2d0690b36afc9
Andreas Moreno Jønsson <andreas.ecuador@live.dk>
2023-05-24 20:22:18 +0200

    Klar til produktion

--- frontend/src/ApolloProvider.js
+++ frontend/src/ApolloProvider.js
@@ -10,7 +10,7 @@ import {
 import { setContext } from "@apollo/client/link/context";
 
 const httpLink = createHttpLink({
-  uri: "http://localhost:9000",
+  uri: "https://fjaesbog-gateway.azurewebsites.net/",
 });
 
 const authLink = setContext(() => {
